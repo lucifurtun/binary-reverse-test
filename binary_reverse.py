a46b9fe02e7c0d0b96491fd4d81a84bf42dd2c9c
Luci Furtun <lucianfurtun@gmail.com>
2020-12-03 15:30:42 +0200

    Handle case when input file does not exist and add some logging

--- binary_reverse.py
+++ binary_reverse.py
@@ -1,14 +1,24 @@
 import argparse
+import logging
 import os
 from pathlib import Path
+from typing import Optional
+
+logging.basicConfig(level=logging.INFO)
+logger = logging.getLogger(__name__)
 
 FIRST_BYTE_POSITION = 0
 BUFFER_SIZE = 1024
 
 
-def generate_reversed_file(input_file_path: Path) -> Path:
+def generate_reversed_file(input_file_path: Path) -> Optional[Path]:
+    try:
+        file_size = input_file_path.stat().st_size
+    except FileNotFoundError:
+        logger.error('File "%s" does not exist. Skipping...', input_file_path)
+        return None
+
     output_file_path = input_file_path.parent / f'{input_file_path.stem}_output{input_file_path.suffix}'
-    file_size = input_file_path.stat().st_size
     bytes_to_read = BUFFER_SIZE if BUFFER_SIZE < file_size else file_size
     index = -bytes_to_read
 
@@ -30,6 +40,8 @@ def generate_reversed_file(input_file_path: Path) -> Path:
 
             index -= bytes_to_read
             input_file.seek(index, os.SEEK_END)
+
+    logger.info('File "%s" was reversed successfully. Output file: "%s"', input_file_path, output_file_path)
 
     return output_file_path
 
